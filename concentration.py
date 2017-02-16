edcbd14a24b9fa10d250719c7a7858079210b074
mderakhsh <mderakh@colostate.edu>
2017-02-15 19:11:59 -0700

    Update concentration.py

--- concentration.py
+++ concentration.py
@@ -1,3 +1,6 @@
+# print "This program is written to compute concentration of " \
+#       "solute in Molar unit with a given box size and number of solutes"
+
 # X is box size, N is number of molecules, C is concentration
 N=24
 X=15.65
@@ -5,4 +8,8 @@ X=15.65
 C=(N/((X**3)*(1e-27)))*(1./1000.)*(1./6.02e23)
 
 
-print "For a box size of", X,"and", N, "molecules of solute","the molar concentration of solute is:", C
+# print "For a box size of", X,"and", N, "molecules of solute","the molar concentration is:", C
+print "\n"
+print "\nNumber of molecules =", N
+print "\nBox size =", X, "^ 3 (nm)^3"
+print "\nMolar concentration =", C, "M"
